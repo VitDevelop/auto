97c6e518f5ff67ae1eb1b336dd373dcdc22c2dfd
Balanici Vitalie <balanici_vitalik@mail.ru>
2015-10-02 18:16:44 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+   // The rest above this is hidden
+   $('.slider').slider({full_width: true,
+   						indicators: false});
+
+   $('.modal-trigger').leanModal();
+  }); // end of document ready
+
+})(jQuery); 
